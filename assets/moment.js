8b88de22e16d02b68f087defe4fa711ec8292082
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-01-07 19:43:10 -0600

    adding data values to firebase

--- assets/moment.js
+++ assets/moment.js
@@ -9,5 +9,39 @@ console.log("we are linked");
     projectId: "train-scheduler-57137",
     storageBucket: "train-scheduler-57137.appspot.com",
     messagingSenderId: "249437400168"
+
   };
+
   firebase.initializeApp(config);
+
+  //variables 
+
+  var database = firebase.database(); 
+
+  var trainName ="";
+  var destination = "";
+  var firstTrainTime= "";
+  var frequency = "";
+
+  //functions and events 
+
+  //when submit button is clicked
+  // step 1: send to data base 
+  //step 2: display to screen 
+
+  $("#submit-button").on("click", function() {
+      console.log("I've been clicked");
+      event.preventDefault();
+      database.ref().set({
+        "trainName" : trainName,
+        "destination": destination,
+        "firstTrainTime": firstTrainTime,
+        "frequency" : frequency
+      });
+    
+      //when i try to do that, permission denied 
+
+
+
+  });
+
