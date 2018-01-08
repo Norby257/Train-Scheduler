cd7977f1ff23cc892b77ce31550f287b4eec693d
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-01-07 19:55:06 -0600

    added value event, simplified id names

--- assets/moment.js
+++ assets/moment.js
@@ -26,22 +26,43 @@ console.log("we are linked");
   //functions and events 
 
   //when submit button is clicked
-  // step 1: send to data base 
-  //step 2: display to screen 
+  // step 1: get user input from form fields
+  //step 2: send to database
+  //step 3: set up db so will print changes when they occur
+  //step 4: update the HTML / DOM accordingly  --will have to create TR and then be smart about selecting which rows to update 
 
   $("#submit-button").on("click", function() {
       console.log("I've been clicked");
       event.preventDefault();
+      
+
+      //get the user input 
+      trainName = $("#name-input").val().trim();
+      destination = $("#destination-input").val().trim();
+      firstTrainTime = $("#first-train-input").val().trim();
+      frequency = $("#frequency-input").val().trim();
+
+
       database.ref().set({
         "trainName" : trainName,
         "destination": destination,
         "firstTrainTime": firstTrainTime,
         "frequency" : frequency
       });
-    
-      //when i try to do that, permission denied 
-
-
-
   });
 
+    //step 3: set up db so will print changes when they occur
+
+database.ref().on("value", function(snapshot) {
+    //as a test, console log initial data
+    console.log(snapshot.val());
+
+    // as a test, log data of all properies 
+    console.log(snapshot.val().trainName);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().phone);
+})
+
+        //when i try to do that, permission denied - need to fix rules in firebase to test what's going on 
+
+ 
