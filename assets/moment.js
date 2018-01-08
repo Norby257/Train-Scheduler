afc683133b5924de80380196f84353a6dc653c83
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-01-07 19:02:00 -0600

    added HTML structure(tables and forms), links to JS libraries and firebase

---
+++ assets/moment.js
@@ -0,0 +1,13 @@
+console.log("we are linked");
+
+//connect to firebase 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCSDjfq81VJhKOIKmTA3ANeCMXrNdSaeX8",
+    authDomain: "train-scheduler-57137.firebaseapp.com",
+    databaseURL: "https://train-scheduler-57137.firebaseio.com",
+    projectId: "train-scheduler-57137",
+    storageBucket: "train-scheduler-57137.appspot.com",
+    messagingSenderId: "249437400168"
+  };
+  firebase.initializeApp(config);
