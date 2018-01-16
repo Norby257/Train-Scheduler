aba18c56356e4bb3500b1b1a3de480f6a029fe31
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-01-16 17:51:24 -0600

    pseudocode for moment.js methods

--- assets/moment.js
+++ assets/moment.js
@@ -1,22 +1,4 @@
-//pseudo - code 
-/* style this similar to cta app or google some transit apps 
-users who are admin(hint hint authenticated) can fill out the form with the train data
-and then it updates it real time:
-a) current time - frequency = time of next arrival in HH:MM:SS format 
 
- /am or pm but I thought it was military time?
-b)
-
-
-//the bonus challenges 
-
-
-
-//update and remove buttons for each train - function + for loop 
-
-//firebase authentications so only ppl who log into their google and github accounts can log in
-
-*/ 
 console.log("we are linked");
 
 //connect to firebase 
@@ -70,19 +52,20 @@ console.log("we are linked");
       //console logging to test this 
 
       console.log(newTrain);
-    
+     //pseudo code 
       //set up start and nextArrival vars for moment.js 
       var start = firstTrainTime;
       var end = frequency;
+      //use moment methods to get the difference in minutes from first train time and frequency 
       var minutesAway = moment(end).diff(moment(start));
       console.log(minutesAway);
+      //put in easy to read format such as " train is x minutes away" to avoid concatenation
       var humanize = moment.duration(minutesAway).humanize();
       console.log(humanize);
       var asMinutes = moment.duration(minutesAway).as('minutes');
       
     
 
-      
 
   //step 2: send to database/write data to data base 
 
@@ -117,9 +100,9 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
 // 3. add train data to the table 
 $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td></tr>");
+//then here we would append the data from moment.js  
+  //5. display that on screen for upcoming arrival times 
 
-  // 4.now do this with moment.js to calculate the next arrival 
-  //5. display that on screen 
 
 
   })
